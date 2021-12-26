754bf3127262a30b2b396fc8ef6346cb630c6c89
matvs <6883643+matvs@users.noreply.github.com>
2021-12-26 02:45:23 +0100

    Advent of Code: Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,33 @@
+import readInputInt
+import readTestInputInt
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun countIncreasedFn(prevValueArg: Int): (Int, Int) -> Int {
+        var prevValue = prevValueArg
+        return fun(counter: Int, value: Int): Int {
+            val counter = if (value > prevValue)  counter + 1 else counter
+            prevValue = value
+            return counter
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun countIncreasedDepths(data: List<Int>): Int {
+        return data.slice(IntRange(1, data.size - 1)).fold(0, countIncreasedFn(data[0]))
+    }
+
+    fun part1(input: List<Int>): Int {
+        return countIncreasedDepths(input)
+    }
+
+    fun part2(input: List<Int>): Int {
+        return countIncreasedDepths(input.windowed(3,1).map({window: List<Int> -> window.sum()}))
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readTestInputInt("day1")
+    check(part1(testInput) == 7)
 
-    val input = readInput("Day01")
+    val input = readInputInt("Day01")
     println(part1(input))
     println(part2(input))
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -5,7 +5,15 @@ import java.security.MessageDigest
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt").readLines()
+fun readData(path:String, name: String): List<String> = File(path, "$name.txt").readLines()
+
+fun readInput(name: String): List<String> = readData("src/resources/input", name)
+
+fun readTestInput(name: String): List<String> = readData("src/resources/test", name)
+
+fun readInputInt(name: String) = readInput(name).map { x: String -> x.toInt() }
+
+fun readTestInputInt(name: String) = readTestInput(name).map { x: String -> x.toInt() }
 
 /**
  * Converts string to md5 hash.
